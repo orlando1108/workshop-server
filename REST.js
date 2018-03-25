4a8b8751ee2222e832b68cdc9ce93b922bcb34d6
orlando1108 <erwan.raulo2015@campus-eni.fr>
2018-03-25 16:03:08 +0200

    first

---
+++ REST.js
@@ -0,0 +1,175 @@
+var mysql = require("mysql");
+
+var insertedCommande_ID;
+
+function REST_ROUTER(router,connection,md5) {
+    var self = this;
+    self.handleRoutes(router,connection,md5);
+}
+REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
+    
+    router.post("/commande",function(req,res){
+
+        
+
+     /*   connection.query( 'SELECT * FROM some_table', ( err, rows ) => {
+            connection.query( 'SELECT * FROM other_table', ( err, rows2 ) => {
+              connection.close( err => {
+                // ... do something with all the results
+              }
+            }
+          }
+
+*/
+        var query = "INSERT INTO ??(??) VALUES (?)";
+        var table = ["Commande","id_user",req.body.id_user]; //md5(req.body.password)
+        
+        var msg_collections;
+        var msg_produits;
+        query = mysql.format(query,table);
+        //connection.connect();
+        connection.query(query,function(err,rows){
+            
+            if(err) {
+                throw err;
+            } else {
+                insertedCommande_ID = rows.insertId;
+                console.log("inserted id commande  " + insertedCommande_ID);
+            }
+            
+            
+        
+            if(req.body.list_produits){
+               /* console.log("list produits exists");
+                console.log("inserted id commande  " + insertedCommande_ID);*/
+                for (i = 0; i < req.body.list_produits.length; i++) { 
+                    console.log("id PRODUIT before  " + req.body.list_produits[i]);
+                    table = ["Produit_Commande","id_produit","id_commande",Number(req.body.list_produits[i]),insertedCommande_ID ]; //md5(req.body.password)
+                    console.log("id PRODUIT after  " + table);
+                    query = "INSERT INTO ??(??,??) VALUES (?,?)";
+                    query = mysql.format(query,table);
+                    //connection.connect();
+                    connection.query(query,function(err,rows){
+                    if(err) {
+                        throw err;
+                    }
+                });
+                }
+                msg_produits = " products added ";
+    
+            }
+            if(req.body.list_collections){
+                console.log("list collections exists");
+               // console.log("inserted id commande  " + insertedCommande_ID);
+               for (i = 0; i < req.body.list_collections.length; i++) { 
+                    table = ["Collection_Commande","id_collection","id_commande",Number(req.body.list_collections[i]),insertedCommande_ID ]; //md5(req.body.password)
+                    query = "INSERT INTO ??(??,??) VALUES (?,?)";
+                    query = mysql.format(query,table);
+                   // connection.connect();
+                    connection.query(query,function(err,rows){
+                    if(err) {
+                        throw err;
+                    } else {
+                        msg_collections = " collections added ";
+                    }
+                   });
+               }
+    
+            }
+        });
+        res.json({"Error" : false, "msg_collections" : msg_collections, "msg_produits" : msg_produits });
+
+    });
+
+    router.post("/user",function(req,res){
+        var query = "INSERT INTO ??(??,??) VALUES (?,?)";
+        var table = ["User","email","entreprise",req.body.email, req.body.factory]; //md5(req.body.password)
+        query = mysql.format(query,table);
+        connection.connect();
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query", "Detail": JSON.stringify(err)});
+            } else {
+                res.json({"Error" : false, "Message" : "User Added !"});
+            }
+        });
+    });
+
+   /* router.get("/",function(req,res){
+        res.json({"Message" : "Hello World !"});
+
+    });
+*/
+    router.get("/collections",function(req,res){
+        var query = "SELECT * FROM ??";
+        var table = ["Collection"];
+        query = mysql.format(query,table);
+        connection.getConnection(function(err, connection) {
+        connection.query(query,function(err,rows){
+            connection.release();
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query",  "Detail": JSON.stringify(err)});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Collections" : rows});
+            }
+        });
+    });
+    });
+
+    router.get("/produits/:id_collection",function(req,res){
+        var query = "SELECT * FROM ?? WHERE ??=?";
+        var table = ["Produit","id_collection",req.params.id_collection];
+        query = mysql.format(query,table);
+        connection.getConnection(function(err, connection) {
+        connection.query(query,function(err,rows){
+            connection.release();
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Produits" : rows});
+            }
+        });
+    });
+    });
+    router.get("/users/:user_id",function(req,res){
+        var query = "SELECT * FROM ?? WHERE ??=?";
+        var table = ["user_login","user_id",req.params.user_id];
+        query = mysql.format(query,table);
+        connection.connect();
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Users" : rows});
+            }
+        });
+    });
+
+  /*  router.put("/users",function(req,res){
+        var query = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
+        var table = ["user_login","user_password",md5(req.body.password),"user_email",req.body.email];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Updated the password for email "+req.body.email});
+            }
+        });
+    });*/
+
+   /* router.delete("/users/:email",function(req,res){
+        var query = "DELETE from ?? WHERE ??=?";
+        var table = ["user_login","user_email",req.params.email];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Deleted the user with email "+req.params.email});
+            }
+        });
+    });*/
+}
+
+module.exports = REST_ROUTER;
---
+++ server.js
@@ -0,0 +1,105 @@
+
+/*var mysql = require('mysql');
+var db;
+function connection() {
+  if (!db) {
+    db = mysql.createConnection({
+      host: "127.0.0.1",
+      port: "3306",
+      user: "alvin",
+      password: "starwars17",
+      database: "starwars"
+    });
+    db.connect(function (err) {
+      if (!err) {
+        console.log('Database is connected!');
+      } else {
+        console.log('Error connecting database!' + err);
+      }
+    });
+    db.on('error', function (err) {
+      console.log('db error', err);
+      if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+        db = null;
+        connection()                         // lost due to either server restart, or a
+      } else {                                      // connnection idle timeout (the wait_timeout
+        throw err;                                  // server variable configures this)
+      }
+    });
+  }
+  return db;
+}
+module.exports = connection();
+*/
+
+var express = require("express");
+var mysql   = require("mysql");
+var bodyParser  = require("body-parser");
+var md5 = require('MD5');
+var rest = require("./REST.js");
+var app  = express();
+
+function REST(){
+    var self = this;
+    self.connectMysql();
+};
+
+REST.prototype.connectMysql = function() {
+    var self = this;
+    var pool      =    mysql.createPool({
+        connectionLimit : 100,
+        host     : 'bbqgq84xq-mysql.services.clever-cloud.com',
+        user     : 'uyiyi3ppu3ygjfe0',
+        password : 'xOB2HwbAefRcAmHzwsW',
+        database : 'bbqgq84xq',
+        debug    :  false
+    });
+    /*pool.getConnection(function(err,connection){
+        if(err) {
+          self.stop(err);
+        } else {
+          self.configureExpress(connection);
+        }
+    });*/
+    self.configureExpress(pool);
+}
+
+REST.prototype.configureExpress = function(connection) {
+      var self = this;
+      app.use(bodyParser.urlencoded({ extended: true }));
+      app.use(bodyParser.json());
+      var router = express.Router();
+      app.use('/api', router);
+      var rest_router = new rest(router,connection,md5);
+      
+      app.use(function (req, res, next) {
+          // Website you wish to allow to connect
+          res.setHeader('Access-Control-Allow-Origin', '*');
+          // Request methods you wish to allow
+          res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+          // Request headers you wish to allow
+          res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization, Content-Type');
+          // Set to true if you need the website to include cookies in the requests sent
+          // to the API (e.g. in case you use sessions)
+          res.setHeader('Access-Control-Allow-Credentials', true);
+          // Pass to next layer of middleware
+          next();
+        
+    
+      
+});
+self.startServer();
+}
+
+REST.prototype.startServer = function() {
+      app.listen(3000,function(){
+          console.log("All right ! I am alive at Port 8080.");
+      });
+}
+
+REST.prototype.stop = function(err) {
+    console.log("ISSUE WITH MYSQL n" + err);
+    process.exit(1);
+}
+
+new REST();
