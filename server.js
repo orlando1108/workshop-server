c5630476dea7ed2a3cf1e7c41a53c1b0b14c2856
orlando1108 <erwan.raulo2015@campus-eni.fr>
2018-03-25 21:04:23 +0200

    fix cors

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ var mysql   = require("mysql");
 var bodyParser  = require("body-parser");
 var md5 = require('MD5');
 var rest = require("./REST.js");
+var cors = require('cors');
 var app  = express();
 
 function REST(){
@@ -71,6 +72,7 @@ REST.prototype.configureExpress = function(connection) {
       var router = express.Router();
       app.use('/api', router);
       var rest_router = new rest(router,connection,md5);
+	  app.use(cors());
       
       app.use(function (req, res, next) {
           // Website you wish to allow to connect
@@ -78,7 +80,7 @@ REST.prototype.configureExpress = function(connection) {
           // Request methods you wish to allow
           res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
           // Request headers you wish to allow
-          res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization, Content-Type');
+          res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
           // Set to true if you need the website to include cookies in the requests sent
           // to the API (e.g. in case you use sessions)
           res.setHeader('Access-Control-Allow-Credentials', true);
